751190a876a10285c2f68f184858347db1cb2323
CHAPUIS GAETAN p1612068 <p1612068@univ-lyon1.fr>
2017-02-16 17:43:16 +0100

    Rajout du goldenmaster

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -14,36 +14,33 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
-            Game aGame = new Game();
 
-            aGame.add("Chet");
-            aGame.add("Pat");
-            aGame.add("Sue");
-
-            Random rand = new Random();
-
-            do
+            for (int i = 0; i < 100; i++)
             {
+                Game aGame = new Game();
 
-                aGame.roll(rand.Next(5) + 1);
+                aGame.add("Chet");
+                aGame.add("Pat");
+                aGame.add("Sue");
 
-                if (rand.Next(9) == 7)
+                Random rand = new Random(i);
+
+                do
                 {
-                    notAWinner = aGame.wrongAnswer();
-                }
-                else
-                {
-                    notAWinner = aGame.wasCorrectlyAnswered();
-                }
+                    aGame.roll(rand.Next(5) + 1);
 
-
-
-            } while (notAWinner);
-
+                    if (rand.Next(9) == 7)
+                    {
+                        notAWinner = aGame.wrongAnswer();
+                    }
+                    else
+                    {
+                        notAWinner = aGame.wasCorrectlyAnswered();
+                    }
+                    Console.WriteLine(i);
+                } while (notAWinner);
+            }
         }
-
-
     }
-
 }
 
