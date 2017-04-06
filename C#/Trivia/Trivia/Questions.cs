2b07d620f9f8ddd3de2ec9782a797ef1e7956c6f
CHAPUIS GAETAN p1612068 <p1612068@univ-lyon1.fr>
2017-04-06 16:56:46 +0200

    Creation de la classe question

---
+++ C#/Trivia/Trivia/Questions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    class Questions
+    {
+        private readonly Dictionary<int, string> _categories = new Dictionary<int, string>() { { 0, "Pop" }, { 1, "Science" }, { 2, "Sports" }, { 3, "Rock" } };
+        
+        LinkedList<string> popQuestions = new LinkedList<string>();
+        LinkedList<string> scienceQuestions = new LinkedList<string>();
+        LinkedList<string> sportsQuestions = new LinkedList<string>();
+        LinkedList<string> rockQuestions = new LinkedList<string>();
+
+
+
+        public void AskQuestion(int currentPlacePlayer)
+        {
+            Console.WriteLine("The category is " + CurrentCategory(currentPlacePlayer));
+            if (CurrentCategory(currentPlacePlayer) == "Pop")
+            {
+                Console.WriteLine(popQuestions.First());
+                popQuestions.RemoveFirst();
+            }
+            if (CurrentCategory(currentPlacePlayer) == "Science")
+            {
+                Console.WriteLine(scienceQuestions.First());
+                scienceQuestions.RemoveFirst();
+            }
+            if (CurrentCategory(currentPlacePlayer) == "Sports")
+            {
+                Console.WriteLine(sportsQuestions.First());
+                sportsQuestions.RemoveFirst();
+            }
+            if (CurrentCategory(currentPlacePlayer) == "Rock")
+            {
+                Console.WriteLine(rockQuestions.First());
+                rockQuestions.RemoveFirst();
+            }
+        }
+        public string CurrentCategory(int currentPlacePlayer)
+        {
+            return _categories[currentPlacePlayer % 4];
+        }
+                
+        public string CreateRockQuestion(int index)
+        {
+            return "Rock Question " + index;
+        }
+
+        public void Generate()
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                popQuestions.AddLast("Pop Question " + i);
+                scienceQuestions.AddLast(("Science Question " + i));
+                sportsQuestions.AddLast(("Sports Question " + i));
+                rockQuestions.AddLast(CreateRockQuestion(i));
+            }
+        }
+    }
+}
